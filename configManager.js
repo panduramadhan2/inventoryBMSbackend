02073e395650cab06e81d1e287a9b47ce56484e6
panduramadhan2 <panduramadhan2@gmail.com>
2023-07-31 22:15:08 +0700

    fixing configmanager dirname

--- configManager.js
+++ configManager.js
@@ -1,6 +1,29 @@
-// configManager.js
+// // configManager.js
+// import fs from 'fs';
+// import path from 'path';
+
+// const configFile = path.join(__dirname, './serviceAccountKey.json');
+
+// function loadServiceAccountKey() {
+//     try {
+//         const serviceAccountKey = fs.readFileSync(configFile, 'utf8');
+//         return JSON.parse(serviceAccountKey);
+//     } catch (error) {
+//         console.error('Error loading serviceAccountKey.json:', error);
+//         return null;
+//     }
+// }
+
+// const serviceAccount = loadServiceAccountKey();
+
+// export { serviceAccount }; // Use export instead of module.exports
+
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const configFile = path.join(__dirname, './serviceAccountKey.json');
 
@@ -16,4 +39,4 @@ function loadServiceAccountKey() {
 
 const serviceAccount = loadServiceAccountKey();
 
-export { serviceAccount }; // Use export instead of module.exports
+export { serviceAccount };
